b54a1a22704e51749c167fe43e55db17f79055c5
Daniel D <daniel.diepold@gmail.com>
2014-02-07 19:32:06 +0100

    Save audio files to disk and reuse

--- mpd_youtube.py
+++ mpd_youtube.py
@@ -1,18 +1,20 @@
 import urllib2
+import os
 from subprocess import Popen, PIPE
 
 from youtube_dl import YoutubeDL
-from flask import Flask, Response, request, abort, render_template, stream_with_context, flash, url_for
+from flask import Flask, Response, request, abort, render_template, stream_with_context, flash, url_for, make_response
 from werkzeug.utils import secure_filename
 from mpd import MPDClient
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
+app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'not so secret'
-app.config["MPD_HOST"] = "localhost"
-app.config["MPD_PORT"] = "6600"
+app.config['MPD_HOST'] = "localhost"
+app.config['MPD_PORT'] = "6600"
 app.config['MPD_PASSWORD'] = ""
 app.config['SERVER_URL'] = 'http://localhost'
+app.config['DOWNLOAD_FOLDER'] = '/tmp'
 
 ydl = YoutubeDL({
     'forceurl': True,
@@ -41,16 +43,35 @@ def stream():
         vurl = 'http://www.youtube.com/watch?v=' + request.args.get('v')
 
     yt_info = get_info(vurl)
-    
+
+    if app.config['DOWNLOAD_FOLDER']:
+        cached_file_path = os.path.join(app.config['DOWNLOAD_FOLDER'], secure_filename(yt_info['title'] + '.mp3'))
+        if os.path.exists(cached_file_path):
+            print "Serving from cache."
+            return make_response(open(cached_file_path, 'rb').read())
+
     def generate():
-        p = Popen(["ffmpeg", "-i", get_url(yt_info), "-f", "mp3", "-vn", "-ab", "256k", "-strict", "-2", "-"],
-                  stdout=PIPE)
-        output = p.stdout.read(256*128)
-        while output:
-            yield output
+        cache_file = None
+        cache_filename = os.path.join(app.config['DOWNLOAD_FOLDER'], secure_filename(yt_info['title'] + '.mp3'))
+        try:
+            if app.config['DOWNLOAD_FOLDER']:
+                cache_file = open(cache_filename + '.part', 'wb')
+            p = Popen(["ffmpeg", "-i", get_url(yt_info), "-f", "mp3", "-vn", "-ab", "256k", "-strict", "-2", "-"],
+                      stdout=PIPE)
             output = p.stdout.read(256*128)
-        p.stdout.close()
-        p.wait()
+            while output:
+                if cache_file:
+                    cache_file.write(output)
+                yield output
+                output = p.stdout.read(256*128)
+            if cache_file:
+                cache_file.close()
+                os.rename(cache_filename + '.part', cache_filename)
+        finally:
+            if cache_file:
+                cache_file.close()
+            p.stdout.close()
+            p.wait()
     return Response(stream_with_context(generate()),
                     headers={
                         "Content-Type": "audio/mp3",
